0b68a7f90426816665c3055c0c75337c8ecfe445
dipanshusolanki <deepanshusolanki@gmail.com>
2024-06-27 16:33:34 +0530

    Transaction Component Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import WalletDetail from "./components/WalletDetail";
+import TransactionItem from "./components/TransactionItem";
 
 function App() {
+  const [walletBalance, setWalletBalance] = useState(0);
+  const [expensesAmount, setExpensesAmount] = useState(0);
+  const [transactionList, setTransactionList] = useState([
+    { title: "Samosa", date: Date.now(), amount: 150 },
+    { title: "Pizza", date: Date.now(), amount: 250 },
+  ]);
+  const [expensesList, setExpensesList] = useState([
+    { category: "Entertainment" },
+    { category: "Travel" },
+  ]);
+
+  useEffect(() => {
+    const loadAmounts = () => {
+      //Getting Wallet Balance on Initial Load
+      if (localStorage.getItem("WalletBalance") === null) {
+        localStorage.setItem("WalletBalance", 5000);
+        setWalletBalance(localStorage.getItem("WalletBalance"));
+      } else {
+        setWalletBalance(localStorage.getItem("WalletBalance"));
+      }
+
+      //Getting Expenses Amount on Initial Load
+      if (localStorage.getItem("ExpenseAmount") === null) {
+        localStorage.setItem("ExpenseAmount", 0);
+        setExpensesAmount(localStorage.getItem("ExpenseAmount"));
+      } else {
+        setExpensesAmount(localStorage.getItem("ExpenseAmount"));
+      }
+    };
+
+    loadAmounts();
+  }, []);
+
   const handleWalletButtonClick = (type) => {
     console.log(type);
   };
@@ -15,7 +50,7 @@ function App() {
             <div className="balanceandexpenses">
               <WalletDetail
                 title={"Wallet Balance"}
-                amount={"4500"}
+                amount={walletBalance}
                 theme={"green"}
                 buttontext={"+ Add Income"}
                 clickHandler={handleWalletButtonClick}
@@ -23,7 +58,7 @@ function App() {
               />
               <WalletDetail
                 title={"Expenses"}
-                amount={"500"}
+                amount={expensesAmount}
                 theme={"red"}
                 buttontext={"+ Add Expense"}
                 clickHandler={handleWalletButtonClick}
@@ -35,7 +70,14 @@ function App() {
           <div className="bottom">
             <div className="transactions">
               <h2 className="secondaryheading">Recent Transactions</h2>
-              <div className="transactioncontent"></div>
+              <div className="transactioncontent">
+                {transactionList.map((transaction) => (
+                  <TransactionItem
+                    key={transaction.date}
+                    transaction={transaction}
+                  />
+                ))}
+              </div>
             </div>
             <div className="expenses">
               <h2 className="secondaryheading">Top Expenses</h2>
--- src/components/TransactionItem.jsx
+++ src/components/TransactionItem.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./TransactionItem.css";
+import { PiPizzaThin } from "react-icons/pi";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import { GoPencil } from "react-icons/go";
 
-const TransactionItem = () => {
-  return <></>;
+const TransactionItem = ({ transaction }) => {
+  return (
+    <>
+      <div className="transactionItem">
+        <div className="transactionDetails">
+          <div className="transactionIcon">
+            <PiPizzaThin />
+          </div>
+          <div className="transactionTitle">
+            <h3 className="transactionName">{transaction.title}</h3>
+            <p className="transactionDate">{transaction.date}</p>
+          </div>
+        </div>
+        <div className="transactionActions">
+          <p className="transactionAmount">&#8377;{transaction.amount}</p>
+          <div className="actionButtons">
+            <button className="removeTransaction">
+              <IoIosCloseCircleOutline />
+            </button>
+            <button className="editTransaction">
+              <GoPencil />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="transactionDivider" />
+    </>
+  );
 };
 
 export default TransactionItem;
